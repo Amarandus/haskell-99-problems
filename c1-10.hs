e10f8a9ba699ad0100eaf8d8cb934cb4f45f0415
Amarandus <carl.schneider+github@ruhr-uni-bochum.de>
2019-10-28 19:18:04 +0100

    Solve first 10 problems

--- c1-10.hs
+++ c1-10.hs
@@ -69,26 +69,89 @@ test06 = [ c06 [1,2,3] == False
          , c06 "madamimadam" == True
          , c06 [1,2,4,8,16,8,4,2,1] == True]
 
+-- Problem 7
+-- Flatten a nested list structure.
+data NestedList a = Elem a | List [NestedList a]
+
+c07 :: NestedList a -> [a]
+c07 (Elem x) = [x]
+c07 (List []) = []
+c07 (List (x:xs)) = (c07 x) ++ (c07 (List xs))
+
 test07 :: [Bool]
-test07 = []
+test07 = [ c07 (Elem 5) == [5]
+         , c07 (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]]) == [1,2,3,4,5]
+         , (length $ c07 (List [])) == 0 -- Work around missing Eq a constraint for empty lists
+         ]
+
+-- Problem 8
+-- Eliminate consecutive duplicates of list elements. 
+
+c08 :: Eq a => [a] -> [a]
+c08 [] = []
+c08 (x:[]) = [x]
+c08 (x:xs)
+  | x == head (c08 xs) = c08 xs
+  | otherwise = [x] ++ c08 xs
 
 test08 :: [Bool]
-test08 = []
+test08 = [ c08 "aaaabccaadeeee" == "abcade"]
+
+-- Problem 9
+-- Pack consecutive duplicates of list elements into sublists.
+-- If a list contains repeated elements they should be placed in separate sublists. 
+
+splitC :: Eq a => [a] -> ([a], [a])
+splitC [] = ([], [])
+splitC (x:[]) = ([x], [])
+splitC (x:xs) = (x : firstGroup, remainder)
+  where
+    firstGroup = takeWhile (== x) xs
+    remainder = drop (length firstGroup) xs
+
+iterate' :: ([a] -> ([a], [a])) -> ([[a]], [a]) -> ([[a]], [a])
+iterate' fn (x, []) = (x, [])
+iterate' fn (x, y)  = iterate' (fn) (x ++ [fst r], snd r)
+  where
+    r = fn y
+
+c09 :: Eq a => [a] -> [[a]]
+c09 [] = []
+c09 x = fst groupedList
+  where
+    groupedList = iterate'
+                  (splitC)
+                  ([], x)
+
 
 test09 :: [Bool]
-test09 = []
+test09 = [ c09 ['a', 'a', 'a', 'a', 'b', 'c', 'c',
+                'a', 'a', 'd', 'e', 'e', 'e', 'e']
+           == ["aaaa","b","cc","aa","d","eeee"]
+         ]
+
+-- Problem 10
+-- Run-length encoding of a list.
+-- Use the result of problem P09 to implement the so-called run-length encoding
+-- data compression method. Consecutive duplicates of elements are encoded as
+-- lists (N E) where N is the number of duplicates of the element E. 
+
+c10 :: Eq a => [a] -> [(Int, a)]
+c10 [] = []
+c10 x = map (\x -> (length x, head x)) $ c09 x
 
 test10 :: [Bool]
-test10 = []
+test10 = [ c10 "aaaabccaadeeee" == [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
+         ]
 
 main = do
-     putStr $ unlines $ map (runTest) [ test01
-                                      , test02
-                                      , test03
-                                      , test04
-                                      , test05
-                                      , test06
-                                      , test07
-                                      , test08
-                                      , test09
-                                      , test10]
+  putStr $ unlines $ map (runTest) [ test01
+                                   , test02
+                                   , test03
+                                   , test04
+                                   , test05
+                                   , test06
+                                   , test07
+                                   , test08
+                                   , test09
+                                   , test10]
