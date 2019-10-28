c2e6956bd41fb33772d122fba32a5ab2a3eb2a79
Amarandus <carl.schneider+github@ruhr-uni-bochum.de>
2019-10-28 19:17:43 +0100

    Catch missing testcases

--- Test.hs
+++ Test.hs
@@ -1,6 +1,7 @@
 module Test (runTest) where
 
 runTest :: [Bool] -> String
+runTest [] = "No test cases"
 runTest x = if all (id) x
             then "Test succeeded"
             else "Test failed"
